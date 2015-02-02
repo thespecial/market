deb5bc880ce002267fcf3a8d9d31fd70c293e930
Ilya Smotrov <ismotrov@Ilyas-MacBook-Pro.local>
2015-02-02 12:36:18 -0800

    final commit

--- lib/sites/market/pages/categories_page.rb
+++ lib/sites/market/pages/categories_page.rb
@@ -3,7 +3,7 @@ require 'taza/page'
 
 module Market
   class CategoriesPage < ::Taza::Page
-    filter :top_block?, :top5links
+    filter :top_block?, :top5
     
     element(:top5) {browser.div(:class => 'top5categ')}
 
